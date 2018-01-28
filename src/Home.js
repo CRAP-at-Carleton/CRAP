cf27ce05e0c0295df1029a715c316a9595a0597c
Simon Bilsky-Rollins <simonbr73@gmail.com>
2018-01-27 22:13:24 -0600

    setup react-router and basic homepage

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        Home page
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Home from './Home';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home} />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(router, document.getElementById('root'));
 registerServiceWorker();
