b7a29f09f0dc08f3eee322042228a0bff9e50a15
Dustin Michels <dustin7538@gmail.com>
2018-01-28 08:31:32 -0600

    working on sidebar

---
+++ src/components/homepage/NavTabs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './HeaderBar.css'
+
+class NavTabs extends React.Component {
+    render() {
+      return (
+        <nav>
+
+        </nav>
+      );
+    }
+  }
+
+export default NavTabs;
