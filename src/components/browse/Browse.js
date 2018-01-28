8153fd7f2470ca4043be4861b2f69a28ffb78be0
Simon Bilsky-Rollins <simonbr73@gmail.com>
2018-01-28 12:15:37 -0600

    browse redo and other misc. CSS stuff

--- src/components/browse/Browse.js
+++ src/components/browse/Browse.js
@@ -2,19 +2,37 @@ import React from 'react';
 import HeaderBar from '../homepage/HeaderBar'
 import SideBar from './SideBar'
 import Grid from './Grid'
-// import './Browse.css';
+import './Browse.css';
 
 class Browse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFilters: {
+        dept: [],
+        type: [],
+        period: []
+      }
+    };
+    this.handleFilterClick = this.handleFilterClick.bind(this);
+  }
+
+  handleFilterClick(type, value, selected) {
+    
+  }
+
   render() {
     // let depts = ['A', 'B', 'C', 'D', 'E', 'F'];
     let periods = ['1', '2', '3', '4', '5', '6', '7','8','9','0','1','2','3','4','5','6', '7','8','9','0','1','2','3','4','5'];
     // let types = ['100', '200', '300', 'D', 'Off-Campus', 'Comps'];
 
     return (
-      <div>
+      <div class="browse">
         <HeaderBar />
-        <SideBar />
-        <Grid courses={periods}/>
+        <div class="main">
+          <SideBar />
+          <Grid courses={periods}/>
+        </div>
       </div>
     );
   }
