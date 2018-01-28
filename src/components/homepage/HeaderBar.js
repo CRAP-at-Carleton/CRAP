797167ba6a6b5f634c7b36a21a6123fe845140b3
Dustin Michels <dustin7538@gmail.com>
2018-01-28 10:01:23 -0600

    add nav-bar

--- src/components/homepage/HeaderBar.js
+++ src/components/homepage/HeaderBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './HeaderBar.css'
+import NavTabs from "./NavTabs";
 
 class HeaderBar extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class HeaderBar extends React.Component {
       <div className="header">
         <h1><a href='/'>CRAP@Carleton</a></h1>
         <h2>"Course Registration and Planning"</h2>
+        <NavTabs/>
       </div>
     )
   }
---
+++ src/components/homepage/NavTabs.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+class Navigation extends React.Component {
+  render() {
+    return(
+      <div>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">CRAP@Carleton</a>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                {/* Change from a to Link */}
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/browse">Browse</Link></li>
+                <li><Link to="/saved">Saved</Link></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Navigation;
