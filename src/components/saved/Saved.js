c8a90468e7de84b1a3608d0780b4226d4e227b9e
Dustin Michels <dustin7538@gmail.com>
2018-01-28 12:14:13 -0600

    modified

--- src/components/saved/Saved.js
+++ src/components/saved/Saved.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import HeaderBar from '../homepage/HeaderBar'
+import HeaderBar from '../homepage/HeaderBar';
+
+// import Globalize from 'globalize';
+// import globalizeLocalizer from 'react-widgets-globalize';
+import { Calendar } from 'react-widgets';
+
+// Globalize.locale('en');
+// globalizeLocalizer();
+
+let widget = (
+  <Calendar
+    dateFormat={dt => String(dt.getDate())}
+  />
+);
 
 class Saved extends React.Component {
   render() {
     return (
       <div>
         <HeaderBar />
-        <h1>This is a calendar!</h1>
-        <button> Browse All </button>
+        <widget/>
       </div>
     );
   }
